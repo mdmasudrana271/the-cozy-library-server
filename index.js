39174166c42d0ebc5a25bdec92d361fe02af748a
md masud rana <mr7123619@gmail.com>
2022-11-25 00:30:33 +0600

    connect uri on database and make user post route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 const app = express();
@@ -9,6 +10,33 @@ const app = express();
 
 app.use(cors())
 app.use(express.json())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.8tifwil.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+    try{
+        const usersCollection = client.db('the-cozy-library').collection("users")
+
+        app.post('/users', async(req, res)=>{
+            const user = req.body;
+            const result = await usersCollection.insertOne(user)
+            res.send(result)
+        })
+    }
+    finally{
+
+    }
+}
+
+run().catch(error=> {
+    console.log(error.message)
+})
 
 
 
